bb7599bbd7e3e0ab0bb70b19d55bdc6f40fe74bf
samuele301197 <parisisamu32@gmail.com>
2025-05-09 16:07:19 +0200

    feat:btn

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const emailsElem = document.getElementById("emails");
+const btnElem = document.getElementById("btn");
 
 
-
+btnElem.addEventListener("click", () => {
+    emailsElem.innerHTML = "";
    for (let i = 0; i < 10; i++){
 axios
   .get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((resp) => {
     const mail = resp.data.response;
-emailsElem.innerHTML += `<li class="list-group-item">${mail}</li>`;    
+    emailsElem.innerHTML += `<li class="list-group-item">${mail}</li>`;    
   });
 }
+})
+
